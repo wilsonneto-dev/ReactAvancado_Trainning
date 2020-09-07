c6e9b1af077ff1739aec7938d11d867abd1ddd3d
Wilson Neto <contato@wilsonneto.com.br>
2020-09-07 08:24:57 -0300

    Add RTL config

---
+++ front/src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+const Main: React.FC = () => {
+  return (
+    <div>
+      <main>
+        <h1>React Avançado</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Main;
---
+++ front/src/components/Main/test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />);
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div>
+        <main>
+          <h1>
+            React Avançado
+          </h1>
+        </main>
+      </div>
+    `);
+  });
+});
